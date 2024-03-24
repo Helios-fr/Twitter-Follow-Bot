a0eb8897afe78e816a83d7b8769f259410f17d76
Helios <email@example.com>
2024-03-24 18:29:15 +1000

    rename main.py to test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+'''
+This file was used to workshop the base features of the bot before it was implemented into the main program
+'''
+from twitter.account import Account
+from utils import read_secret, get_id, read_secrets
+from httpx import Client
+
+# Authenticate to Twitter
+secret = read_secrets()
+with open("proxy.secret") as f:
+    proxy = f.read()
+accounts = {}
+for id in secret:
+    accounts[id] = Account(cookies={"ct0": secret[id]["ct0"], "auth_token": secret[id]["auth_token"]}, session=Client(proxies={"https": proxy}))
+print(accounts)
+
+# get the user id of @wired
+id = get_id("wired")
+for account in accounts.values():
+    account.follow(id)
+    print(f"Followed @wired from @{list(accounts.keys())[list(accounts.values()).index(account)]}")
