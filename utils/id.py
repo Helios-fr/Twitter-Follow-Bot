f717ce38fc8a9b7c5a3ea8fd02490e8564650057
Helios <email@example.com>
2024-03-24 21:10:52 +1000

    update username to id to a more reliable api

--- utils/id.py
+++ utils/id.py
@@ -1,7 +1,4 @@
 def get_id(username: str) -> str:
     import requests
-    response = requests.post('https://tweeterid.com/ajax.php', data={'input': username})
-    return response.text
-
-if __name__ == "__main__":
-    print(get_id("wired"))
+    response = requests.get('https://twitvd.com/twuserid.php', params={'username': 'DrDemography'})
+    return response.json()["data"]["user_id"]
