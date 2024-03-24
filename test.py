2465f399f9bc3d6f422b2a92648299b5ea9096e5
Helios <email@example.com>
2024-03-24 19:32:23 +1000

    fix proxy issue in test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 This file was used to workshop the base features of the bot before it was implemented into the main program
 '''
-from twitter.account import Account
+from utils.twitter.account import Account
 from utils import read_secret, get_id, read_secrets
 from httpx import Client
 
@@ -11,11 +11,11 @@ with open("proxy.secret") as f:
     proxy = f.read()
 accounts = {}
 for id in secret:
-    accounts[id] = Account(cookies={"ct0": secret[id]["ct0"], "auth_token": secret[id]["auth_token"]}, session=Client(proxies={"http://": f"http://{proxy}"}))
+    accounts[id] = Account(cookies={"ct0": secret[id]["ct0"], "auth_token": secret[id]["auth_token"]}, session=Client(proxies={"https://": f"http://{proxy}", "http://": f"http://{proxy}"}))
 print(accounts)
 
 # get the user id of @wired
 id = get_id("wired")
 for account in accounts.values():
-    account.follow(id)
+    r = account.follow(id)
     print(f"Followed @wired from @{list(accounts.keys())[list(accounts.values()).index(account)]}")
