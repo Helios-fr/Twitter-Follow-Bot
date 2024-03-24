6223a69c8f985b543ab4c492490f4d4dc02cbed6
Helios <email@example.com>
2024-03-24 18:48:39 +1000

    finished testing proxies

--- proxy_test.py
+++ proxy_test.py
@@ -1,7 +1,11 @@
 import requests
+from requests.auth import HTTPProxyAuth
+# post without proxy
+r = requests.get("https://api.ipify.org")
+print(r.text)
 # post with proxy and check ip
 with open("proxy.secret") as f:
     proxy = f.read()
 print(proxy)
-r = requests.get("https://api.ipify.org", proxies={"https": proxy})
+r = requests.get("http://api.ipify.org", proxies={"http": f"http://{proxy}", "https": f"https://{proxy}"})
 print(r.text)
