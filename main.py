1147a1243bb7ca1978e00745b5e8381c47bdef05
Helios <email@example.com>
2024-03-24 20:47:01 +1000

    simple entering in main.py

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ proxy = {
 
 mgr = Manager(proxy=None)
 
-mgr.follow("wired", amount=1)
+# enter the username of the account you want to follow
+username = input("Enter the username of the account you want to follow: ") or "wired"
+amount = int(input("Enter the number of accounts you want to use to follow (-1 for all): ") or 1)
+
+mgr.follow(username=username, amount=amount)
