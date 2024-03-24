c7414305018dec7ebcbb21fa2c9f6c84a02688c2
Helios <email@example.com>
2024-03-24 19:33:19 +1000

    update read_secrets to accept keys_dir

--- utils/secret.py
+++ utils/secret.py
@@ -3,10 +3,10 @@ def read_secret(id):
     with open(os.path.join("keys", f"{id}.secret")) as f:
         return dict(line.strip().split(" = ") for line in f)
 
-def read_secrets():
+def read_secrets(keys_dir="keys"):
     import os
     secrets = {}
-    for file in os.listdir("keys"):
+    for file in os.listdir(keys_dir):
         if file.endswith(".secret"):
             id = file.removesuffix(".secret")
             secrets[id] = read_secret(id)
