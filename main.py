7b066366b37d59200f904c39fb2f681728544808
Helios <79072303+Helios-fr@users.noreply.github.com>
2024-03-25 11:29:39 +1000

    add proxy support to main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from utils import Manager
 
-'''
-proxy = {
-    "http": "http://username:password@proxy:port",
-}
-'''
-
-mgr = Manager(proxy=None)
-
 # enter the username of the account you want to follow
 username = input("Enter the username of the account you want to follow: ") or "wired"
 amount = int(input("Enter the number of accounts you want to use to follow (-1 for all): ") or 1)
+proxy = input("Enter the proxy you want to use (leave blank for no proxy): ")
+if proxy:
+    proxy = {
+        "http": f"http://{proxy}",
+        "https": f"http://{proxy}",
+    }
+else:
+    proxy = None
 
+mgr = Manager(proxy=proxy)
 mgr.follow(username=username, amount=amount)
